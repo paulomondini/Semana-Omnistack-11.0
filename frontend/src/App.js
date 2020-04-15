436441f13a754c223878cfd8a1195bfc5d36196d
paulomondini <paulo.1mondini@gmail.com>
2020-04-14 21:53:55 -0300

    front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './global.css';
+import Routes from './routes';
 
 function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
 
